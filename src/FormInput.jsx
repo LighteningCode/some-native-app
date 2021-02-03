b1bfbca760d7bc44ba23f39c2eb289ba56a30880
Elvis Agbesi <elvisagbesi@gmail.com>
2021-02-03 08:06:37 +0000

    feat: not completed

--- src/FormInput.jsx
+++ src/FormInput.jsx
@@ -2,18 +2,69 @@ import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
 import { Button, StyleSheet, Switch, Text, View, Dimensions, pick, FlatList, Modal } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { color } from 'react-native-reanimated';
 
 import tailwind from "tailwind-rn";
 
 
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22,
+        backgroundColor: 'transparent',
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        height: 200,
+        width: 150,
+        justifyContent: 'space-around',
+        borderRadius: 10,
+        padding: 25,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 20
+        },
+        shadowOpacity: 0.15,
+        shadowRadius: 30.84,
+        elevation: 5
+    },
+    selectbuttonActive: {
+        textAlign: "center",
+        fontWeight: 'bold',
+        width: 100,
+        paddingHorizontal: 150,
+        paddingVertical: 12
+    },
+    selectbuttondeactivated: {
+        textAlign: "center",
+        fontWeight: 'bold',
+        color: "black",
+        width: 100,
+        paddingHorizontal: 150,
+        paddingVertical: 12
+    }
+})
+
 function FormInput({ }) {
 
     const [isModalVisible, setIsModalVisible] = useState(false)
+    const [formData, setFormData] = useState({ firstname: "", lastname: "", gender: "male", age: "" })
     const formControl = tailwind("border border-black w-full py-3 px-2 rounded-lg mb-5")
 
     const openModal = () => {
         setIsModalVisible(true)
     }
+
+    const changeText = (text, name) => {
+        setFormData(prevState => ({ ...prevState, [name]: text }))
+    }
+
+
 
     return (
         <>
@@ -21,13 +72,14 @@ function FormInput({ }) {
             <Modal
                 visible={isModalVisible}
                 transparent={true}>
-                
-                <View>
-                    <Text style={{textAlign: 'center'}}>Select gender</Text>
-                    <Picker style={{ height: 10, width: 100, marginBottom: 50 }}>
-                        <Picker.Item label="Male" value="male" />
-                        <Picker.Item label="Female" value="female" />
-                    </Picker>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={{ textAlign: 'center' }}>Select gender</Text>
+                        <Picker style={{ height: 10, width: 100, marginBottom: 0, backgroundColor: "red" }}>
+                            <Picker.Item label="Male" value="male" />
+                            <Picker.Item label="Female" value="female" />
+                        </Picker>
+                    </View>
                 </View>
             </Modal>
 
@@ -37,19 +89,20 @@ function FormInput({ }) {
                         <Text style={tailwind('text-blue-800 font-semibold')}>Form input</Text>
                     </View>
 
-                    <Text>Enter Name:</Text>
-                    <TextInput style={formControl} value={"10"} />
+                    <Text>Enter firstname:</Text>
+                    <TextInput style={formControl} onChangeText={(text) => changeText(text, 'firstname')} value={formData.firstname} />
+
+                    <Text>Enter lastname:</Text>
+                    <TextInput style={formControl} onChangeText={(text) => changeText(text, 'lastname')} value={formData.lastname} />
 
                     <Text>Enter Age:</Text>
-                    <TextInput style={formControl} value={"10"} />
+                    <TextInput style={formControl} onChangeText={(text) => changeText(text, 'age')} value={formData.age} />
 
-                    <Text>Enter date of gender:</Text>
-                    <View style={tailwind(`w-full px-10 py-3 rounded mb-10`)}>
-                        <TouchableOpacity style={formControl} onPress={openModal}>
-                            <Text style={{ textAlign: "center", fontWeight: 'bold', color: "black" }}>Select gender</Text>
-                        </TouchableOpacity>
+                    <Text>Select gender:</Text>
+                    <View style={{...tailwind("rounded-lg"),flexDirection:"row", marginVertical: 15, width:250,height:40, backgroundColor:"transparent",justifyContent:"space-around", alignItems: 'center',borderColor: "blue",borderWidth:1}}>
+                        <TouchableOpacity><Text style={{color:"blue"}}>Male</Text></TouchableOpacity>
+                        <TouchableOpacity><Text style={{color:"blue"}}>Female</Text></TouchableOpacity>
                     </View>
-
 
 
                     <TouchableOpacity style={tailwind("bg-blue-500 w-64 px-10 py-3 rounded")}>
